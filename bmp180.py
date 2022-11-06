c999ba8fbcfb97a1b4c8aff9f27e668376ae0b26
octaprog7 <goctaprog@gmail.com>
2022-11-06 14:27:14 +0500

    + two properties: 1. temp_or_press 2. oversample adapted to the latest version of sensor_pack. removed _check_oss function.

--- bmp180.py
+++ bmp180.py
@@ -5,7 +5,7 @@ import micropython
 import array
 
 from sensor_pack import bus_service
-from sensor_pack.base_sensor import BaseSensor, Iterator
+from sensor_pack.base_sensor import BaseSensor, Iterator, check_value
 
 
 # ВНИМАНИЕ: не подключайте питание датчика к 5В, иначе датчик выйдет из строя! Только 3.3В!!!
@@ -13,16 +13,9 @@ from sensor_pack.base_sensor import BaseSensor, Iterator
 
 
 @micropython.native
-def _check_oss(oss_val: int) -> int:
-    if not 0 <= oss_val <= 3:
-        raise ValueError(f"Invalid oversample settings: {oss_val}")
-
-    return oss_val
-
-
-@micropython.native
 def get_conversion_cycle_time(temperature_or_pressure: bool, oversample_settings: int) -> int:
-    """возвращает время преобразования в [мс] датчиком температуры или давления в зависимости от его настроек"""
+    """возвращает время преобразования в [мс] датчиком температуры или давления в зависимости от его настроек.
+    returns the conversion time in [ms] by a temperature or pressure sensor depending on its settings"""
     delays_ms = 5, 8, 14, 26
     if temperature_or_pressure:
         return delays_ms[0]    # temperature
@@ -31,7 +24,9 @@ def get_conversion_cycle_time(temperature_or_pressure: bool, oversample_settings
 
 
 def _calibration_regs_addr() -> iter:
-    """возвращает итератор с адресами внутренних регистров датчика, хранящих калибровочные коэффициенты """
+    """возвращает итератор с адресами внутренних регистров датчика, хранящих калибровочные коэффициенты.
+    returns an iterator with the addresses of the sensor's internal registers that
+    store the calibration coefficients."""
     return range(0xAA, 0xBF, 2)
 
 
@@ -47,20 +42,20 @@ class Bmp180(BaseSensor, Iterator):
         super().__init__(adapter, address, True)
         # self.adapter = adapter
         #
-        self.temp_or_press = True
-        self.press4 = None  # for precalculate
-        self.press3 = None  # for precalculate
-        self.press2 = None  # for precalculate
-        self.press1 = None  # for precalculate
-        self.press0 = None  # for precalculate
-        self.tmp1 = None    # for precalculate
-        self.tmp0 = None    # for precalculate
-        self.B5 = None      # for precalculate
-        self.oss = _check_oss(oversample_settings)
-        self.adr = address
-        # self.i2c = i2c
+        self._temp_or_press = True
+        self._press4 = None  # for precalculate
+        self._press3 = None  # for precalculate
+        self._press2 = None  # for precalculate
+        self._press1 = None  # for precalculate
+        self._press0 = None  # for precalculate
+        self._tmp1 = None    # for precalculate
+        self._tmp0 = None    # for precalculate
+        self._B5 = None      # for precalculate
+        #
+        self.oversample = oversample_settings
         # массив, хранящий калибровочные коэффициенты (11 штук)
-        self.cfa = array.array("l")  # signed long elements
+        # array storing calibration coefficients (11 elements)
+        self._cfa = array.array("l")  # signed long elements
         # считываю калибровочные коэффициенты
         self._read_calibration_data()
         # предварительный расчет
@@ -76,38 +71,35 @@ class Bmp180(BaseSensor, Iterator):
     def get_calibration_data(self, index: int) -> int:
         """возвращает калибровочный коэффициент по его индексу (0..10).
         returns the calibration coefficient by its index (0..10)"""
-        if not 0 <= index < 11:
-            raise ValueError(f"Invalid index value: {index}")
-        return self.cfa[index]
+        check_value(index, range(0, 11), f"Invalid index value: {index}")
+        return self._cfa[index]
 
     @micropython.native
     def precalculate(self):
-        """предварительно вычисленные значения"""
-        # для расчета температуры
-        self.tmp0 = self.get_calibration_data(4) / 2 ** 15  #
-        self.tmp1 = self.get_calibration_data(9) * 2 ** 11  #
-        # для расчета давления
-        self.press0 = self.get_calibration_data(7) / 2 ** 23
-        self.press1 = self.get_calibration_data(1) / 2 ** 11
-        self.press2 = self.get_calibration_data(2) / 2 ** 13
-        self.press3 = self.get_calibration_data(6) / 2 ** 28
-        self.press4 = abs(self.get_calibration_data(3)) / 2 ** 15
+        """предварительно вычисленные значения. precomputed values"""
+        # для расчета температуры/for temperature calculation
+        self._tmp0 = self.get_calibration_data(4) / 2 ** 15  #
+        self._tmp1 = self.get_calibration_data(9) * 2 ** 11  #
+        # для расчета давления/for pressure calculation
+        self._press0 = self.get_calibration_data(7) / 2 ** 23
+        self._press1 = self.get_calibration_data(1) / 2 ** 11
+        self._press2 = self.get_calibration_data(2) / 2 ** 13
+        self._press3 = self.get_calibration_data(6) / 2 ** 28
+        self._press4 = abs(self.get_calibration_data(3)) / 2 ** 15
 
     def _read_calibration_data(self) -> int:
         """Читает калибровочные значение из датчика.
-        read calibration values from sensor.
-        return count read values"""
-        if len(self.cfa):
+        read calibration values from sensor. return count read values"""
+        if len(self._cfa):
             raise ValueError(f"calibration data array already filled!")
         for index, addr in enumerate(_calibration_regs_addr()):
-            reg_val = self.adapter.read_register(self.address, addr, 2)     # _read_register(addr, 2)
-            # reg_val = self._read_register(addr, 2)
+            reg_val = self.adapter.read_register(self.address, addr, 2)
             rv = self.unpack("H" if 2 < index < 6 else "h", reg_val)[0]
             # check
             if rv == 0x00 or rv == 0xFFFF:
                 raise ValueError(f"Invalid register addr: {addr} value: {hex(rv)}")
-            self.cfa.append(rv)
-        return len(self.cfa)
+            self._cfa.append(rv)
+        return len(self._cfa)
 
     def get_id(self) -> int:
         """Возвращает идентификатор датчика. Правильное значение - 0х55.
@@ -125,36 +117,32 @@ class Bmp180(BaseSensor, Iterator):
         """Start measurement process in sensor.
         Если temperature_or_pressure==Истина тогда будет выполнен запуск измерения температуры иначе давления!
         Вы должны подождать результата 5 мс после запуска измерения температуры.
-        Время ожидания результата после запуска измерения давления зависит от переменной self.осс.
+        Время ожидания результата после запуска измерения давления зависит от переменной self._оss.
         self.оss     задержка, мс
         0               5
         1               8
         2               14
-        3               26
-
-        Внимание! Глупо ждать результата методом time.delay_ms(value)!
-        Вместо этого можно занять процессор полезным делом!"""
-        self.temp_or_press = temperature_or_pressure
-        loc_oss = self.oss
+        3               26"""
+        self._temp_or_press = temperature_or_pressure
+        loc_oss = self.oversample
         start_conversion = 0b00100000   # bit 5 - запуск преобразования (1)
         bit_4_0 = 0x14  # давление
         if temperature_or_pressure:
             bit_4_0 = 0x0E  # температура
             loc_oss = 0  # обнуляю OSS при температуре
         val = loc_oss << 6 | start_conversion | bit_4_0
-        # self._write_register(0xF4, val, 1)
         self._write_register(0xF4, val, 1)
 
     @micropython.native
     def get_temperature(self) -> float:
         """возвращает значение температуры, измеренное датчиком в Цельсиях.
         returns the temperature value measured by the sensor in Celsius"""
-        # raw = self._read_register(0xF6, 2)  # считывание сырого значения
+        # считывание сырого значения
         raw = self.adapter.read_register(self.address, 0xF6, 2)
         temp = self.unpack("H", raw)[0]  # unsigned short
-        a = self.tmp0 * (temp - self.get_calibration_data(5))
-        b = self.tmp1 / (a + self.get_calibration_data(10))
-        self.B5 = a + b  #
+        a = self._tmp0 * (temp - self.get_calibration_data(5))
+        b = self._tmp1 / (a + self.get_calibration_data(10))
+        self._B5 = a + b  #
         return 6.25E-3 * (a + b + 8)
 
     @micropython.native
@@ -170,22 +158,22 @@ class Bmp180(BaseSensor, Iterator):
         Better call the method in pairs:
         get_temperature
         get_pressure"""
-        # raw = self._read_register(0xF6, 3)  # считывание сырого значения (три байта)
+        # считывание сырого значения (три байта)
         raw = self.adapter.read_register(self.address, 0xF6, 3)
         msb, lsb, xlsb = raw
-        uncompensated = ((msb << 16)+(lsb << 8)+xlsb) >> (8-self.oss)
-        b6 = self.B5-4000
-        x1 = self.press0 * b6 ** 2  #
-        x2 = self.press1 * b6
+        uncompensated = ((msb << 16)+(lsb << 8)+xlsb) >> (8-self.oversample)
+        b6 = self._B5-4000
+        x1 = self._press0 * b6 ** 2  #
+        x2 = self._press1 * b6
         x3 = x1 + x2
-        b3 = (2 + ((x3 + 4 * self.get_calibration_data(0)) * 2**self.oss)) / 4
+        b3 = (2 + ((x3 + 4 * self.get_calibration_data(0)) * 2**self.oversample)) / 4
 
-        x1 = b6 * self.press2
-        x2 = self.press3 * b6 ** 2
+        x1 = b6 * self._press2
+        x2 = self._press3 * b6 ** 2
         x3 = (2+x1+x2) / 4
 
-        b4 = self.press4 * (x3+32768)
-        b7 = (abs(uncompensated)-b3) * (50000 / 2**self.oss)
+        b4 = self._press4 * (x3+32768)
+        b7 = (abs(uncompensated)-b3) * (50000 / 2**self.oversample)
 
         curr_pressure = 2 * b7 / b4
         x1 = 7.073394953E-7 * curr_pressure ** 2
@@ -200,3 +188,15 @@ class Bmp180(BaseSensor, Iterator):
         if self.temp_or_press:
             return self.get_temperature()
         return self.get_pressure()
+
+    @property
+    def temp_or_press(self) -> bool:
+        return self._temp_or_press
+
+    @property
+    def oversample(self) -> int:
+        return self._oss
+
+    @oversample.setter
+    def oversample(self, value: int):
+        self._oss = check_value(value, range(0, 4), f"Invalid oversample settings: {value}")
